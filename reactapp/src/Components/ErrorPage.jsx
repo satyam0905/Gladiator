efbc9582d6613fc07d15732a9ded2995b034355e
satyam0905 <134535064+satyam0905@users.noreply.github.com>
2026-02-12 12:46:19 +0530

    Add ErrorPage component for error handling

---
+++ reactapp/src/Components/ErrorPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './ErrorPage.css'
+
+
+const ErrorPage = () => {
+  return (
+    <div className='error-page'>
+      <div className='error-container'>
+        <h4>Oops! Something Went Wrong</h4>
+        <br/>
+        <p>Please try again later.</p>
+        <br/>
+        <img src='alert.png' alt="image"></img>
+    </div>
+    </div>
+  )
+}
+
+
+
+
+export default ErrorPage
